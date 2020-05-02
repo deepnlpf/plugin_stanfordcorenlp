17152628a601694f68342db41264b364b7e6d2c1
Francisco Rodrigues <franciscosouzaacer@gmail.com>
2020-05-02 01:37:08 -0300

    Update requeriments.sh

--- requeriments.sh
+++ requeriments.sh
@@ -1,2 +1,9 @@
-# Install Stanza NLP
-pip install stanza -U
+# Install Stanford CoreNLP
+echo "Downloading Stanford CoreNLP.."
+wget http://nlp.stanford.edu/software/stanford-corenlp-4.0.0.zip
+
+echo "Extracting files.."
+unzip stanford-corenlp-4.0.0.zip
+
+# clear.
+rm -r stanford-corenlp-4.0.0.zip
