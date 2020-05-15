e54197ffb8be1d2a8358c0c487f36bc5a983e015
Francisco Rodrigues <franciscosouzaacer@gmail.com>
2020-05-15 10:29:05 -0300

    Add files via upload

--- plugin_stanfordcorenlp.py
+++ plugin_stanfordcorenlp.py
@@ -2,48 +2,45 @@
 # -*- coding: utf-8 -*-
 
 from deepnlpf.core.iplugin import IPlugin
-from deepnlpf.core.boost import Boost
-from deepnlpf.core.output_format import OutputFormat
 
-import json
-
-from os import path
 from corenlp.corenlp import StanfordCoreNLP
 
-class Plugin(IPlugin):
-    
-    def __init__(self, id_pool, lang, document, pipeline):
-        self._id_pool = id_pool
-        self._document = document
-        self._pipeline = pipeline
 
-        path_or_host = path.abspath(path.dirname(__file__)) + "/stanford-corenlp-full-2018-10-05"
-        self.nlp = StanfordCoreNLP(path_or_host)
+class Plugin(IPlugin):
+
+    from os import path
+    PATH_OR_HOST = path.abspath(path.dirname(__file__)) + "/resources"
+
+    def __init__(self, document, pipeline):
+        self._document = document
+
+        self._processors = pipeline["tools"]["stanfordcorenlp"]["processors"]
+        self._lang = pipeline["lang"]
+
+        self.nlp = StanfordCoreNLP(self.PATH_OR_HOST)
 
     def run(self):
-        annotation = Boost().multithreading(self.wrapper, self._document['sentences'])
+        from deepnlpf.core.boost import Boost
+        doc = Boost().multithreading(self.wrapper, self._document)
         self.nlp.close()
-        return self.out_format(annotation)
+        return doc
 
     def wrapper(self, sentence):
-        '''
+        """
             @param annotators : more: https://stanfordnlp.github.io/CoreNLP/annotators.html
             @param pipelineLanguage : en, zh, ar, fr, de, es
             @param outputFormat : json, xml, text, more: https://stanfordnlp.github.io/CoreNLP/human-languages.html
             @param memory : 8g
-        '''
+        """
 
         props = {
-            'timeout': '1500000',
-            'annotators': ', '.join(self._pipeline),
-            'pipelineLanguage': 'en',
-            'outputFormat': 'json'
+            "timeout": "1500000",
+            "annotators": ", ".join(self._processors),
+            "pipelineLanguage": "en",
+            "outputFormat": "json",
         }
-        return json.loads(self.nlp.annotate(sentence, properties=props))
 
-    def out_format(self, annotation):
-        try:
-            return OutputFormat().doc_annotation(_id_pool=self._id_pool, _id_dataset=self._document['_id_dataset'],
-            _id_document=self._document['_id'], tool="stanfordcorenlp", annotation=annotation)
-        except Exception as err:
-            return annotation
+        return self.nlp.annotate(sentence, properties=props)
+
+    def out_format(self, doc):
+        pass
--- requeriments.sh
+++ requeriments.sh
@@ -5,5 +5,7 @@ wget http://nlp.stanford.edu/software/stanford-corenlp-4.0.0.zip
 echo "Extracting files.."
 unzip stanford-corenlp-4.0.0.zip
 
+mv stanford-corenlp-full-2020-04-20 resources
+
 # Clear.
 rm -r stanford-corenlp-4.0.0.zip
