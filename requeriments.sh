cbd1f983f1a15d19e8d0c1d4fcf0fd53036e1840
DylanVsn <43576618+DylanVsn@users.noreply.github.com>
2020-05-19 18:44:16 +0200

    fix wrong dir on mv command

--- requeriments.sh
+++ requeriments.sh
@@ -5,7 +5,8 @@ wget http://nlp.stanford.edu/software/stanford-corenlp-4.0.0.zip
 echo "Extracting files.."
 unzip stanford-corenlp-4.0.0.zip
 
-mv stanford-corenlp-full-2020-04-20 resources
+# Move the extracted folder
+mv $(zipinfo -1 stanford-corenlp-4.0.0.zip | head -1) resources
 
 # Clear.
 rm -r stanford-corenlp-4.0.0.zip
